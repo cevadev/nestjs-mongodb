a3db20a54a286120c45deda3242a912137ef75eb
Carlos Eduardo Villanueva Altuna <carlos.villanueva.altuna@gmail.com>
2021-05-08 19:24:46 -0300

    NestJS Mongo

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,8 +1,10 @@
 import { Module, Global } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
 
 //importamos MongoClient para la conexion a MongoDB
 import { MongoClient } from 'mongodb';
-import { async } from 'rxjs';
+
+import config from '../config';
 
 const API_KEY = '12345634';
 const API_KEY_PROD = 'PROD1212121SA';
@@ -31,19 +33,31 @@ const API_KEY_PROD = 'PROD1212121SA';
       provide: 'MONGO',
       //useFactory hace posible que cualquier servicio pueda inyectar para ello lo incluimos en los exports
       //useFactory utiliza el patron singleton por lo que solo habra una instancia para toda la app
-      useFactory: async () => {
+      //configService-> resolvemos la inyeccion
+      useFactory: async (configService: ConfigType<typeof config>) => {
         //url de conexion, la copiamos desde el mongo compass
-        const uri =
-          'mongodb://root:root@localhost:27017/?authSource=admin&readPreference=primary';
+        //reemplazamos cada una de las variables con el config inyectado
+        const {
+          connection,
+          user,
+          password,
+          host,
+          port,
+          dbname,
+        } = configService.mongo;
+        const uri = `${connection}://${user}:${password}@${host}:${port}/?authSource=admin&readPreference=primary`;
         //creamos una instancia del client
         const client = new MongoClient(uri);
         //ejecutamos el cliente
         await client.connect();
         //indicamos a la BD de queremos conectarnos
-        const database = client.db('platzi-store');
+        //const database = client.db('platzi-store');
+        const database = client.db(`${dbname}`);
         //retornamos la BD como injectable para todos los servicios
         return database;
       },
+      //indicamos las dependencias que vamos a inyectar en el useFactory
+      inject: [config.KEY],
     },
   ],
   exports: ['API_KEY', 'MONGO'],
